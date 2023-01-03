86e5f2fdbc7ad1b92717851fa6ed7822f5744f07
sayjalch <73460966+sayjalch@users.noreply.github.com>
2023-01-03 16:09:02 +0530

    Add files via upload

---
+++ MiniAssignment/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeadingComponent } from './heading/heading.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadingComponent,
+    EmployeeComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ MiniAssignment/src/app/employee/employee.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+//import { Validators} from '@angular/forms';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  @Input() employee: Employee;
+  @Output() onRemoveEmployee = new EventEmitter<number>();
+  @Output() onEditEmployee = new EventEmitter<number>();
+
+  constructor() {
+    this.employee = {
+      firstname: '',
+      lastname: '',
+      birthdate: '',
+      gender: '',
+      education: '',
+      company: '',
+      jobExperience: 0,
+      salary: 0,
+      profile: '',
+    };
+  }
+
+ /* myForm() {
+    this.requiredform = this.fb.group({
+    name: ['', Validators.required ]
+    });
+ }*/
+
+  ngOnInit(): void {
+    console.log(this.employee);
+  }
+
+  deleteEmployeeClicked(){
+    this.onRemoveEmployee.emit(this.employee.id);
+  }
+ editEmployeeClicked(){
+  this.onEditEmployee.emit(this.employee.id);
+ }
+ 
+}
